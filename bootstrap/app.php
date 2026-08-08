acca37ac1a7ad14a6c0567695d896401679f3224
riski ramadhani <riskiramadhani793@gmail.com>
2026-08-08 12:45:56 +0700

    Use modern functions format with includeFiles now that composer.lock is restored

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,22 +22,4 @@ if (isset($_ENV['VERCEL']) || getenv('VERCEL')) {
     $app->useStoragePath('/tmp/storage');
 }
 
-// Bind config repository
-$config = new Illuminate\Config\Repository([
-    'app' => require __DIR__.'/../config/app.php',
-    'view' => require __DIR__.'/../config/view.php',
-    'session' => require __DIR__.'/../config/session.php',
-    'database' => require __DIR__.'/../config/database.php',
-]);
-$app->instance('config', $config);
-
-// Register essential service providers
-$app->register(Illuminate\Events\EventServiceProvider::class);
-$app->register(Illuminate\Filesystem\FilesystemServiceProvider::class);
-$app->register(Illuminate\Routing\RoutingServiceProvider::class);
-$app->register(Illuminate\Cookie\CookieServiceProvider::class);
-$app->register(Illuminate\Session\SessionServiceProvider::class);
-$app->register(Illuminate\View\ViewServiceProvider::class);
-$app->register(Illuminate\Database\DatabaseServiceProvider::class);
-
 return $app;
